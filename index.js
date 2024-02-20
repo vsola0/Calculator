1e03b2bb359ce01693e9b262682d0acfa5598ec0
vsola0 <133784410+vsola0@users.noreply.github.com>
2024-02-20 16:44:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,3 +7,12 @@ function appendToDisplay(input){
 function clearDisplay(){
     display.value = '';
 }
+
+function calculate(){
+    try{
+        display.value = eval(display.value);
+    }
+    catch(error){
+        display.value = 'Error';
+    }
+}
